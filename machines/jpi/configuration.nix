204ac9de7a26668b2d7264ed624a288eea15c020
DavHau <hsngrmpf+github@gmail.com>
2025-06-12 19:57:51 +0700

    add vim

--- machines/jpi/configuration.nix
+++ machines/jpi/configuration.nix
@@ -8,4 +8,8 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDuhpzDHBPvn8nv8RH1MRomDOaXyP4GziQm7r3MZ1Syk dave"
   ];
+
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
 }
